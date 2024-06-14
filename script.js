93ab898181e56da02231f52ccb68bfcf0d2c65c9
asr-404 <akash872609@gmail.com>
2024-06-14 23:47:34 +0530

    Adding personal details and few code in js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed(".typing",{
-    strings:["","Web Designer", "Web Developer"],
+    strings:["","Junior Web Developer","Junior Web Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true

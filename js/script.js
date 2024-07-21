9cd1415e1b550c3261cb573674b17135ff7071be
asr-404 <akash872609@gmail.com>
2024-07-21 23:11:04 +0530

    fixing the navbar and adding the cv download.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ var typed = new Typed(".typing",{
     BackSpeed:60,
     loop:true
 })
+
+// download cv
+document.getElementById('downloadCvBtn').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default action
+
+    var link = document.createElement('a');
+    link.href = '/image.png';
+    link.download = 'CV.png'; // Specify the default name for the downloaded file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
